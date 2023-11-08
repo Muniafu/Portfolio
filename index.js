3ea7554bc18d8e1db7a146eeb416122692703aa7
Muniafu <elambaraka2019@gmail.com>
2023-11-08 13:01:01 +0300

    updates

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-document.querySelector('.btn-primary').addEventListener('click', function() {
-    window.location.href = 'about.html';
-  });  
+const navbarToggle = document.getElementById('navbarToggle');
+const navbar = document.querySelector('.navbar');
+
+navbarToggle.addEventListener('click', function() {
+  navbar.classList.toggle('active');
+});
+const contactForm = document.getElementById('contactForm');
+
+contactForm.addEventListener('submit', function(e) {
+  e.preventDefault(); // Prevent form submission
+
+  // Get form values
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+
+  // Do something with the form values (e.g., send them to a server)
+  console.log('Name:', name);
+  console.log('Email:', email);
+  console.log('Message:', message);
+
+  // Reset the form
+  contactForm.reset();
+});
+function updateTime() {
+  const timeElement = document.getElementById('time');
+  const currentTime = new Date().toLocaleTimeString();
+  timeElement.textContent = currentTime;
+}
+
+setInterval(updateTime, 1000);
